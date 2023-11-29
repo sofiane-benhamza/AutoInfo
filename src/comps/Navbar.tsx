7926b7a51f06b0b991c7f3073cf3528a3d402886
BENHAMZA Sofiane <71550283+sofiane-benhamza@users.noreply.github.com>
2023-11-29 20:39:20 +0100

    Update Navbar.tsx

--- src/comps/Navbar.tsx
+++ src/comps/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
                 <ul className="navbar-nav">
                     <li className="nav-item">
                         <Link to='/'>
-                            <p className={active + ' nav-link text-light '} style={fonted}> Brows<span style={bigger} >e <Icon.CarFrontFill /> C</span>ars
+                            <p className={active + ' nav-link text-light '} style={fonted}> Brows<span style={bigger} >e <Icon.CarFrontFill /> V</span>ehicles
                             </p>
                         </Link>
                     </li>
